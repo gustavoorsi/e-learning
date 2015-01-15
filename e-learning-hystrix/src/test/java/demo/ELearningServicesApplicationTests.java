5f98c04acf2038f88f1d1a23864374ae17028a17
gustavoorsi <gustavoorsi@gmail.com>
2015-01-15 12:14:17 -0300

    Added hystrix project support.

---
+++ e-learning-hystrix/src/test/java/demo/ELearningServicesApplicationTests.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.HystrixApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = HystrixApplication.class)
+public class ELearningServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
