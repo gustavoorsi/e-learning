cf726dd4df8105ec381099a2a79f2c61385e9621
gustavoorsi <gustavoorsi@gmail.com>
2015-03-10 14:27:29 -0300

    ehcache.xml gone
    Replaced ehcache.xml file with a java config class
    "EhCacheTokenConfiguration"

---
+++ e-learning-microservice-rest-2/src/main/java/com/elearning/rest2/config/security/EhCacheTokenConfiguration.java
@@ -0,0 +1,52 @@
+package com.elearning.rest2.config.security;
+
+import net.sf.ehcache.config.CacheConfiguration;
+
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.interceptor.SimpleKeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * Lets configure an eHcache manager that stores authentication tokens.
+ * 
+ * @author Gustavo Orsi
+ *
+ */
+@Configuration
+@EnableCaching
+public class EhCacheTokenConfiguration implements CachingConfigurer {
+
+	@Bean(destroyMethod = "shutdown")
+	public net.sf.ehcache.CacheManager ehCacheManager() {
+		CacheConfiguration cacheConfiguration = new CacheConfiguration();
+		cacheConfiguration.setName("restApiAuthTokenCache");
+		cacheConfiguration.setMemoryStoreEvictionPolicy("LRU");
+		cacheConfiguration.setMaxEntriesLocalHeap(0); // 0 = MAX
+		cacheConfiguration.setTimeToLiveSeconds(14400); // 4 hours.
+		cacheConfiguration.setEternal(false);
+
+		net.sf.ehcache.config.Configuration config = new net.sf.ehcache.config.Configuration();
+		config.addCache(cacheConfiguration);
+
+		return net.sf.ehcache.CacheManager.newInstance(config);
+	}
+
+	@Bean
+	@Override
+	public org.springframework.cache.CacheManager cacheManager() {
+		EhCacheCacheManager cacheManager = new EhCacheCacheManager();
+		cacheManager.setCacheManager(ehCacheManager());
+		return cacheManager;
+	}
+
+	@Override
+	public KeyGenerator keyGenerator() {
+		return new SimpleKeyGenerator();
+	}
+
+}
