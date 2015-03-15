541f64065163a94a75f563b8c7bb005100af16c2
gustavoorsi <gustavoorsi@gmail.com>
2015-03-15 16:49:05 -0300

    web security

--- e-learning-microservice-rest-2/src/main/java/com/elearning/rest2/config/security/EhCacheTokenConfiguration.java
+++ e-learning-microservice-rest-2/src/main/java/com/elearning/rest2/config/security/EhCacheTokenConfiguration.java
@@ -5,6 +5,8 @@ import net.sf.ehcache.config.CacheConfiguration;
 import org.springframework.cache.annotation.CachingConfigurer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.interceptor.CacheErrorHandler;
+import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.cache.interceptor.SimpleKeyGenerator;
 import org.springframework.context.annotation.Bean;
@@ -47,6 +49,18 @@ public class EhCacheTokenConfiguration implements CachingConfigurer {
 	@Override
 	public KeyGenerator keyGenerator() {
 		return new SimpleKeyGenerator();
+	}
+
+	@Override
+	public CacheResolver cacheResolver() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CacheErrorHandler errorHandler() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
